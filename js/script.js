17550354fce0b543f6d4b985e4e0e846264eb901
rweissmann <rweissmann@gmail.com>
2024-05-24 18:36:10 -0300

    VigesimoPrimeraVersion

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ document.querySelector("header").innerHTML=`
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-
 <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -21,7 +20,6 @@ document.querySelector("header").innerHTML=`
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
-  
 </div>
 `
 /*Línea reemplazada, la dejo comentada por si se necesita:
