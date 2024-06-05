73ae1583c766383eda3db5a254812209e3a65e4f
rweissmann <rweissmann@gmail.com>
2024-06-05 20:56:13 -0300

    TreintaAvaVersion

---
+++ BaseDatos/Respaldo/Seguridad.sql
@@ -0,0 +1,9 @@
+use rinconbotanico;
+INSERT INTO usuarios (NombreYApellido, Email, Clave) VALUES (
+'dba', 'rweissmann@gmail.com', 'RWei10669#$',
+'RaulWeissmann', 'rweissmann@gmail.com', 'RWei10669#$',
+'AnaPeyran', 'mapp81@gmail.com', 'ana',
+'PaolaFraticola', 'dgpaofraticola@gmail.com', 'paola',
+'MariaFernandaGallino', 'mfergallino@gmail.com', 'maria',
+'JuanCarlosHelwig', 'TicoHelwig@gmail.com', 'juan');
+SELECT * FROM usuarios;
