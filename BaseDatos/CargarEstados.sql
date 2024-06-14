9e37527bdc5ef6b84255c6fb67cb86b8efcdd36d
rweissmann <rweissmann@gmail.com>
2024-06-14 01:13:27 -0300

    TreintaAvaVersion

---
+++ BaseDatos/CargarEstados.sql
@@ -0,0 +1,4 @@
+use rinconbotanico;
+select * from estado;
+insert into estado (idEstado, DescripEstado) VALUES ("I", "Inactivo");
+insert into estado (idEstado, DescripEstado) VALUES ("A", "Activo");
